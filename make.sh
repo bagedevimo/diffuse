e03a7a03a0df2c0ff5a906b0a81669e140ac1255
Ben Anderson <benanderson@acidic.co.nz>
2019-05-29 20:50:15 +1200

    Empty repo

---
+++ make.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+set -e
+
+cargo build
+sudo cp target/debug/diffuse-server /Users/git/ben-git/git-receive-pack && sudo chown git /Users/git/ben-git/git-receive-pack
+
+GIT_TRACE=2 GIT_CURL_VERBOSE=2 GIT_TRACE_PERFORMANCE=2 GIT_TRACE_PACK_ACCESS=2 GIT_TRACE_PACKET=2 GIT_TRACE_PACKFILE=2 GIT_TRACE_SETUP=2 GIT_TRACE_SHALLOW=2 git push local master
+
+sudo cat /Users/git/out.log
